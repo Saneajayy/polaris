e2a2d36b814e0751a7657dd6c412117771201b4a
ajay <ajaykumarrj302@gmail.com>
2026-02-05 17:39:54 +0530

    created a shadcn project

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+// this function will be used to safely create and merge tailwind classes
